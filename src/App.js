7c38bf001fe431a9e17ecac9826071527bcfdefd
MaaX59 <Maax.strandberg@gmail.com>
2024-03-08 00:20:07 +0100

    animation effect on header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import Navbar from "../src/Sections/Navbar/Navbar.jsx";
 import Header from "../src/Sections/Header/Header.jsx";
 import Subheading from "../src/Sections/Subheading/Subheading.jsx";
+import Shop from "./Sections/Shop/Shop.jsx";
 function App() {
   return (
     <div className="App">
-      <Navbar /> <Header /> <Subheading />
+      <Navbar /> <Header /> <Subheading /> <Shop />
     </div>
   );
 }
---
+++ src/Sections/Shop/Shop.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Shop.css";
+const Shop = () => {
+  return <div className="app_shop">Shop</div>;
+};
+
+export default Shop;
