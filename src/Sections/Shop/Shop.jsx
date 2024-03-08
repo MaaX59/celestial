0e0fc3212bad328e44f4d4ef96c8c2d9c4fc7d08
MaaX59 <Maax.strandberg@gmail.com>
2024-03-08 02:23:00 +0100

    shop done

--- src/Sections/Shop/Shop.jsx
+++ src/Sections/Shop/Shop.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Shop.css";
+import Spacestation from "../../pics/spacestation.jpg";
 const Shop = () => {
-  return <div className="app_shop">Shop</div>;
+  return (
+    <div className="app_shop">
+      <img src={Spacestation} alt="spacestation" />
+      <div className="app_shop_container">
+        <span className="app_shop_title">Shop at Celestial</span>
+        <span className="app_shop_text">
+          Imortilize you journey with Celestial with a memento from our shop
+        </span>
+        <button className="app_shop_container_button"> Shop Now</button>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
