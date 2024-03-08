760b41147859c1dcc8dc40e398a53590728cf647
MaaX59 <Maax.strandberg@gmail.com>
2024-03-08 05:12:09 +0100

    Rooms started

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Navbar from "../src/Sections/Navbar/Navbar.jsx";
 import Header from "../src/Sections/Header/Header.jsx";
 import Subheading from "../src/Sections/Subheading/Subheading.jsx";
 import Shop from "./Sections/Shop/Shop.jsx";
+import Rooms from "./Sections/Rooms/Rooms.jsx";
 function App() {
   return (
     <div className="App">
-      <Navbar /> <Header /> <Subheading /> <Shop />
+      <Navbar /> <Header /> <Subheading /> <Shop /> <Rooms />
     </div>
   );
 }
---
+++ src/Sections/Rooms/Rooms.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Rooms.css";
+
+const Rooms = () => {
+  return (
+    <div className="app_rooms">
+      <div className="app_rooms_title"> Choose Your Destination</div>
+      <div className="app_rooms_content"></div>
+    </div>
+  );
+};
+
+export default Rooms;
