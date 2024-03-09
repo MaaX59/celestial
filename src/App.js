f0a4b54908c1da5f09a5964234319ebda0cbde00
MaaX59 <Maax.strandberg@gmail.com>
2024-03-09 05:31:37 +0100

    Contact

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Header from "../src/Sections/Header/Header.jsx";
 import Subheading from "../src/Sections/Subheading/Subheading.jsx";
 import Shop from "./Sections/Shop/Shop.jsx";
 import Rooms from "./Sections/Rooms/Rooms.jsx";
+import Contact from "./Sections/Contact/Contact.jsx";
 function App() {
   return (
     <div className="App">
-      <Navbar /> <Header /> <Subheading /> <Shop /> <Rooms />
+      <Navbar /> <Header /> <Subheading /> <Shop /> <Rooms /> <Contact />
     </div>
   );
 }
---
+++ src/Sections/Contact/Contact.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return <div className="app_contact">Contact</div>;
+};
+
+export default Contact;
