9fd5de8e7ff9551de7ffdd61172f5e78bfc434ac
MaaX59 <maax.strandberg@gmail.com>
2024-03-14 17:37:07 +0100

    changed color scheme to add more gold

--- src/Sections/Navbar/Navbar.jsx
+++ src/Sections/Navbar/Navbar.jsx
@@ -4,6 +4,19 @@ const Navbar = () => {
   return (
     <div className="app_navbar">
       <span>Celestial Inn</span>
+      <div className="app_navbar-links">
+        <ul>
+          <li>
+            <a href="#destinations">Destinations</a>
+          </li>
+          <li>
+            <a href="#about">About Us</a>
+          </li>
+          <li>
+            <a href="#shop">Shop</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
