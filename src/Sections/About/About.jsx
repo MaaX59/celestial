3789c7c686656d2985270a89322fadfeccab700f
MaaX59 <maax.strandberg@gmail.com>
2024-03-16 11:19:10 +0100

    about section done

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -5,7 +5,7 @@ import market from "../../pics/market.png";
 
 const About = () => {
   return (
-    <div className="app_about">
+    <div className="app_about" id="about">
       <div className="app_about_intro">
         <div className="app_about_title title">Celestial Inn</div>
         <div className="app_about_text text">
@@ -16,24 +16,29 @@ const About = () => {
         <div className="app_about_content_div">
           <div className="app_about_content_div_title">Origin</div>
           <div className="app_about_content_div_text">
-            awdawdada awdawd daw dawd awda dawd awdafse fhfht f awda dawd
-            awdafse fhfht f awda dawd awdafse fhfht f awda dawd awdafse{" "}
+            For more than sixty years, we have delivered unrivaled experiences
+            throughout the world. All the while, evolving our services and
+            continually refining each element of the Celestial experience.
           </div>
           {/* <img src={rocket} alt="Rocket Icon" /> */}
         </div>
         <div className="app_about_content_div">
           <div className="app_about_content_div_title">Preformance</div>
           <div className="app_about_content_div_text">
-            awda dawd awdafse fhfht f awda dawd awdafse fhfht f awda dawd
-            awdafse fhfht f awda dawd awdafse fhfht f awda dawd awdafse fhfht f
+            Our ships are simply the beginning. The perfect vessel for the
+            perfect experience. Expertly engineered and designed, our selection
+            of industry-leading spacecraft brings all corners of the solar
+            system within reach.
           </div>
           {/* <img src={market} alt="marketing icon" /> */}
         </div>
         <div className="app_about_content_div">
           <div className="app_about_content_div_title">Experience</div>
           <div className="app_about_content_div_text">
-            awdd grdgdrgh adawdfg awda dawd awdafse fhfht f awda dawd awdafse
-            fhfht f awda dawd awdafse fhfht f awda dawd awdafse fhfht f
+            Luxury is more than an idea, it’s a feeling.
+            <br /> All our ships are all-inclusive and feature 24-hour butler
+            service to every suite to ensure your stay with us will be
+            unforgettable.
           </div>
           {/* <img src={rocket} alt="Png File - Free Rocket Icon@pngkey.com" /> */}
         </div>
